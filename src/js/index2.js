0a51cc309d5b00378c886a56e782104b7a7fcc38
virchaca <virginia.alvarez82@gmail.com>
2024-01-25 15:32:53 +0100

    js file

--- src/js/index2.js
+++ src/js/index2.js
@@ -1,94 +1,93 @@
-'use strict'; 
+"use strict";
 
-const t1Input = document.querySelector();
-const t2Input = document.querySelector();
-const t3Input = document.querySelector();
-const t4Input = document.querySelector();
+const t1Input = document.querySelector("t1-js");
+const t2Input = document.querySelector("t2-js");
+const t3Input = document.querySelector("t3-js");
+const t4Input = document.querySelector("t4-js");
+const total1Input = document.querySelector("total1-js");
+const total2Input = document.querySelector("total2-js");
 
-    let temp=0;
-    let total1=0;
-    let total2=0;
-    function t1 (){
-        let t1= 7;
-        if (t1> -1 && t1 < 11){
-            if(t1 === 10){
-                console.log("pleno");
-                console.log("esconder boton t2 con hidden")
-                    return (t1);
-                }
-               
-            }else{
-                console.log('Siguiente tirada');
+let temp = 0;
+let total1 = 0;
+let total2 = 0;
 
-                    return (t1);
-                }      
-        };
+function t1() {
+    let t1 = 7;
+    if (t1 > -1 && t1 < 11) {
+      if (t1 === 10) {
+        console.log("pleno");
+        console.log("esconder boton t2 con hidden");
+        return t1;
+      }else {
+      console.log("Siguiente tirada");
+      return t1;
+    }
+  }
+}
+  
+temp = t1();
+console.log(temp);
+total1 += temp;
 
-    temp = t1();
-    console.log(temp);
-    total1 += temp;
-
-    function t2 (t1){
-        let t2= 3;
-        if(t1!==10){
-            if (t2> -1 && t2<=(10-t1)){
-
-                if(t1 + t2 === 10){
-                    console.log("Semi pleno")
-                    return(t2);
-                }else{
-                    console.log("Terminar ronda")
-                    return (t2);}
-                }}
-            };
-    total1 += t2(temp);
-    console.log(total1);
-
-
+function t2(t1) {
+    let t2 = 3;
+    if (t1 !== 10) {
+      if (t2 > -1 && t2 <= (10 - t1)) {
+        if (t1 + t2 === 10) {
+          console.log("Semi pleno");
+          return t2;
+        } else {
+          console.log("Terminar ronda");
+          return t2;
+        }
+      }
+    }
+  }
+  total1 = temp + t2(temp);
+  console.log(total1);
 
 
-    
-    function t3 (){
-        let t3= 7;
-        if (t3> -1 && t1 < 11){
-            
-            if(t3 === 10){
-                console.log("pleno");
-                console.log("esconder boton t2 con hidden")
-                    return (t3);
-                }
-               
-            }else{
-                console.log('Siguiente tirada');
 
-                    return (t3);
-                }      
-        };
 
-    temp = t3();
-    console.log(temp);
-    if(t1===10 || t1+t2===10) {
-        total2 = (temp * 2);
-    }else{total2=temp;}
+  function t3() {
+    let t3 = 7;
+    if (t3 > -1 && t3 < 11) {
+      if (t3 === 10) {
+        console.log("pleno");
+        console.log("esconder boton t2 con hidden");
+        return t3;
+      }else {
+      console.log("Siguiente tirada");
+      return t3;
+    }
+  }
+}
+  
+temp = t3();
+console.log(temp);
+if (t1 === 10 || (t1 + t2) === 10 )
+   { total2 += (temp * 2);}
+else{
+    total2 = temp;
+}
+function t4(t3) {
+    let t4 = 3;
+    if (t3 !== 10) {
+      if (t4 > -1 && t4 <= (10 - t3)) {
+        if (t3 + t4 === 10) {
+          console.log("Semi pleno");
+          return t4;
+        } else {
+          console.log("Terminar ronda");
+          return t4;
+        }
+      }
+    }
+  }
 
-    function t4 (t3){
-        let t4= 3;
-        if(t3!==10){
-            if (t4> -1 && t4<=(10-t3)){
-
-                if(t3 + t4 === 10){
-                    console.log("Semi pleno")
-                    return(t4);}
-                }else{
-                    console.log("Terminar ronda")
-                    return (t4);}
-                }
-            };
-
-            
-
-    total2+= t4(temp);
-    if( t1===10) {
-        total2 += (temp * 2);
-    }else{total2+=temp;}
-    console.log(total2);
+  console.log(total2);
+  if (t1 === 10 )
+   { total2 = (temp + (t4 * 2));}
+else{
+    total2 = temp + t4;
+}
